869c96bc88260dc39890539a0e92bd6cbb1f5a68
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-27 16:04:23 -0300

    feat: v3 smart contract

--- smart-contract/lib/reconhecimento-contract.js
+++ smart-contract/lib/reconhecimento-contract.js
@@ -8,45 +8,40 @@ const { Contract } = require('fabric-contract-api');
 
 class ReconhecimentoContract extends Contract {
 
-    async reconhecimentoExists(ctx, usuarioID) {
+    async usuarioExists(ctx, usuarioID) {
         const buffer = await ctx.stub.getState(usuarioID);
         return (!!buffer && buffer.length > 0);
     }
 
-    async createReconhecimento(ctx, usuarioID, value) {
-        const exists = await this.reconhecimentoExists(ctx, usuarioID);
+    async createUsuario(ctx, usuarioID, value) {
+        const exists = await this.usuarioExists(ctx, usuarioID);
         if (exists) {
-            throw new Error(`The reconhecimento ${usuarioID} already exists`);
+            throw new Error(`The usuario ${usuarioID} already exists`);
         }
-        const asset = { value };
+        const asset = {
+        id: usuarioID,
+        hash: value,
+        saldoRetirada: 0,
+        saldoParaEnviar: 0,
+        };
         const buffer = Buffer.from(JSON.stringify(asset));
         await ctx.stub.putState(usuarioID, buffer);
     }
 
-    async readReconhecimento(ctx, usuarioID) {
-        const exists = await this.reconhecimentoExists(ctx, usuarioID);
+    async readUsuario(ctx, usuarioID) {
+        const exists = await this.usuarioExists(ctx, usuarioID);
         if (!exists) {
-            throw new Error(`The reconhecimento ${usuarioID} does not exist`);
+            throw new Error(`The usuario ${usuarioID} does not exist`);
         }
         const buffer = await ctx.stub.getState(usuarioID);
         const asset = JSON.parse(buffer.toString());
         return asset;
     }
 
-    async updateReconhecimento(ctx, usuarioID, newValue) {
-        const exists = await this.reconhecimentoExists(ctx, usuarioID);
+    async deleteUsuario(ctx, usuarioID) {
+        const exists = await this.usuarioExists(ctx, usuarioID);
         if (!exists) {
-            throw new Error(`The reconhecimento ${usuarioID} does not exist`);
-        }
-        const asset = { value: newValue };
-        const buffer = Buffer.from(JSON.stringify(asset));
-        await ctx.stub.putState(usuarioID, buffer);
-    }
-
-    async deleteReconhecimento(ctx, usuarioID) {
-        const exists = await this.reconhecimentoExists(ctx, usuarioID);
-        if (!exists) {
-            throw new Error(`The reconhecimento ${usuarioID} does not exist`);
+            throw new Error(`The usuario ${usuarioID} does not exist`);
         }
         await ctx.stub.deleteState(usuarioID);
     }
