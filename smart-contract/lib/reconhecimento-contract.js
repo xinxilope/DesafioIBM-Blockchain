af8a25c938777dd3ead07a16b36cd4dc7158b47a
xinxilope <92169100+xinxilope@users.noreply.github.com>
2022-01-31 10:38:04 -0300

    feat: added more validations

--- smart-contract/lib/reconhecimento-contract.js
+++ smart-contract/lib/reconhecimento-contract.js
@@ -1,58 +1,57 @@
 /*
- * SPDX-License-Identifier: Apache-2.0
- */
+* SPDX-License-Identifier: Apache-2.0
+*/
 
 'use strict';
 
 const { Contract } = require('fabric-contract-api');
-let cicloAtivo = false;
 const { v4: uuidv4 } = require('uuid');
+let cicloAtivo = false;
+let pontosRecebidosCiclo = 0;
 
 class ReconhecimentoContract extends Contract {
-    async usuarioExists(ctx, usuarioID) {
-        const buffer = await ctx.stub.getState(usuarioID);
-        return (!!buffer && buffer.length > 0);
-    }
-
-    async createUsuario(ctx, usuarioID, value) {
-        const exists = await this.usuarioExists(ctx, usuarioID);
-        if (exists) {
-            throw new Error(`The usuario ${usuarioID} already exists`);
+    async iniciaCiclo(ctx) {
+        const identity = ctx.clientIdentity;
+        const checkAttr = identity.assertAttributeValue('RH', 'true');
+        if (checkAttr) {
+            if (cicloAtivo === false) {
+                const startKey = '0';
+                const endKey = '999';
+                const iterator = await ctx.stub.getStateByRange(startKey, endKey);
+                cicloAtivo = true;
+                pontosRecebidosCiclo = 0
+        
+        
+                // eslint-disable-next-line no-constant-condition
+                while (true) {
+                    const res = await iterator.next();
+        
+                    if (res.value && res.value.value.toString()) {
+                        const Key = res.value.key;
+        
+                        let usuario = await ctx.stub.getState(Key);
+                        usuario = JSON.parse(usuario);
+                        const asset = {
+                            id: usuario.id,
+                            chave:usuario.chave,
+                            info: usuario.info,
+                            saldoRetirada: usuario.saldoRetirada,
+                            saldoParaEnviar: 2000,
+                        };
+                        const buffer = Buffer.from(JSON.stringify(asset));
+                        await ctx.stub.putState(Key, buffer);
+                    }
+                    if (res.done) {
+                        await iterator.close();
+                        return JSON.stringify('ciclo iniciado!');
+                    }
+                }
+            }  else {
+                throw new Error('Ciclo precisa estar desativado!')
+            }
+        } else {
+            throw new Error('You must be a RH to carry out this transaction!');
         }
-        const asset = {
-            id: usuarioID,
-            chave:uuidv4(),
-            info: value,
-            saldoRetirada: 0,
-            saldoParaEnviar: 0,
-        };
-        const buffer = Buffer.from(JSON.stringify(asset));
-        await ctx.stub.putState(usuarioID, buffer);
-    }
-
-    async readUsuario(ctx, usuarioID) {
-        const exists = await this.usuarioExists(ctx, usuarioID);
-        if (!exists) {
-            throw new Error(`The usuario ${usuarioID} does not exist`);
-        }
-        const buffer = await ctx.stub.getState(usuarioID);
-        const asset = JSON.parse(buffer.toString());
-        return asset;
-    }
-
-    async deleteUsuario(ctx, usuarioID) {
-        const exists = await this.usuarioExists(ctx, usuarioID);
-        if (!exists) {
-            throw new Error(`The usuario ${usuarioID} does not exist`);
-        }
-        await ctx.stub.deleteState(usuarioID);
-    }
-
-    async verificarChave(ctx,usuario){
-        let buffer = await ctx.stub.getState(usuario);
-        const usuinfo = JSON.parse(buffer.toString());
-        let usuchave = usuinfo.chave;
-        return JSON.stringify(usuchave);
     }
 
     async eviarSaldo(ctx, usuarioID1, usuarioChave, usuarioID2, value) {
@@ -71,6 +70,9 @@ class ReconhecimentoContract extends Contract {
             }
             else if (valueIsMultiple !== 0) {
                 throw new Error('The value must be multiple of 100');
+            }
+            else if (pontosRecebidosCiclo >= 2000) {
+                throw new Error('Voce ja recebeu o maximo de pontos disponiveis nesse ciclo (2000 pontos)');
             }
             let usuario1 = await ctx.stub.getState(usuarioID1);
             usuario1 = JSON.parse(usuario1);
@@ -100,57 +102,9 @@ class ReconhecimentoContract extends Contract {
             };
             const buffer2 = Buffer.from(JSON.stringify(asset2));
             await ctx.stub.putState(usuarioID2, buffer2);
+            pontosRecebidosCiclo = pontosRecebidosCiclo + Number(value)
         } else {
             throw new Error('Ciclo precisa estar ativo para fazer transacao!');
-        }
-    }
-
-    async iniciaCiclo(ctx) {
-        const identity = ctx.clientIdentity;
-        const checkAttr = identity.assertAttributeValue('RH', 'true');
-        if (checkAttr) {
-            const startKey = '0';
-            const endKey = '999';
-            const iterator = await ctx.stub.getStateByRange(startKey, endKey);
-            cicloAtivo = true;
-
-
-            // eslint-disable-next-line no-constant-condition
-            while (true) {
-                const res = await iterator.next();
-
-                if (res.value && res.value.value.toString()) {
-                    const Key = res.value.key;
-
-                    let usuario = await ctx.stub.getState(Key);
-                    usuario = JSON.parse(usuario);
-                    const asset = {
-                        id: usuario.id,
-                        chave:usuario.chave,
-                        info: usuario.info,
-                        saldoRetirada: usuario.saldoRetirada,
-                        saldoParaEnviar: 2000,
-                    };
-                    const buffer = Buffer.from(JSON.stringify(asset));
-                    await ctx.stub.putState(Key, buffer);
-                }
-                if (res.done) {
-                    await iterator.close();
-                    return JSON.stringify('ciclo iniciado!');
-                }
-            }
-        } else {
-            throw new Error('You must be a RH to carry out this transaction!');
-        }
-    }
-
-    async finalizaCiclo(ctx) {
-        const identity = ctx.clientIdentity;
-        const checkAttr = identity.assertAttributeValue('RH', 'true');
-        if (checkAttr) {
-            cicloAtivo = false;
-        } else {
-            throw new Error('You must be a RH to carry out this transaction!');
         }
     }
 
@@ -206,6 +160,66 @@ class ReconhecimentoContract extends Contract {
         } else {
             throw new Error('Ciclo precisa estar ativo para fazer transacao!');
         }
+    }
+
+    async finalizaCiclo(ctx) {
+        const identity = ctx.clientIdentity;
+        const checkAttr = identity.assertAttributeValue('RH', 'true');
+        if (checkAttr) {
+            if (cicloAtivo === true) {
+                cicloAtivo = false;
+            }  else {
+                throw new Error('Ciclo precisa estar ativo!');
+            }
+        } else {
+            throw new Error('You must be a RH to carry out this transaction!');
+        }
+    }
+
+    async verificarChave(ctx,usuario){
+        let buffer = await ctx.stub.getState(usuario);
+        const usuinfo = JSON.parse(buffer.toString());
+        let usuchave = usuinfo.chave;
+        return JSON.stringify(usuchave);
+    }
+
+    async usuarioExists(ctx, usuarioID) {
+        const buffer = await ctx.stub.getState(usuarioID);
+        return (!!buffer && buffer.length > 0);
+    }
+
+    async createUsuario(ctx, usuarioID, value) {
+        const exists = await this.usuarioExists(ctx, usuarioID);
+        if (exists) {
+            throw new Error(`The usuario ${usuarioID} already exists`);
+        }
+        const asset = {
+            id: usuarioID,
+            chave:uuidv4(),
+            info: value,
+            saldoRetirada: 0,
+            saldoParaEnviar: 0,
+        };
+        const buffer = Buffer.from(JSON.stringify(asset));
+        await ctx.stub.putState(usuarioID, buffer);
+    }
+
+    async readUsuario(ctx, usuarioID) {
+        const exists = await this.usuarioExists(ctx, usuarioID);
+        if (!exists) {
+            throw new Error(`The usuario ${usuarioID} does not exist`);
+        }
+        const buffer = await ctx.stub.getState(usuarioID);
+        const asset = JSON.parse(buffer.toString());
+        return asset;
+    }
+
+    async deleteUsuario(ctx, usuarioID) {
+        const exists = await this.usuarioExists(ctx, usuarioID);
+        if (!exists) {
+            throw new Error(`The usuario ${usuarioID} does not exist`);
+        }
+        await ctx.stub.deleteState(usuarioID);
     }
 }
 
